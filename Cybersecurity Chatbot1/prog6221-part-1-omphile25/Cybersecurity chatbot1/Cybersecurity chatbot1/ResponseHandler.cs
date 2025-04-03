7fd3c1b77c9ad1d5df3c6902c72ed39b1cef8c87
omphile25 <omphilemoale25@gmail.com>
2025-04-03 23:34:10 +0200

    Added the voice greeting and Asciiart methods

---
+++ Cybersecurity Chatbot1/prog6221-part-1-omphile25/Cybersecurity chatbot1/Cybersecurity chatbot1/ResponseHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cybersecurity_chatbot1
+{
+    class ResponseHandler
+    {
+
+    }
+}
